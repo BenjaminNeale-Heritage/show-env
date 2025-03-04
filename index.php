35799fabaaee73fad01f90b0f0c05c27fcfbda1a
Benjamin Neale <neale.b@heritage.com.au>
2025-03-05 08:44:12 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
   print "Hello, World! I am running on host " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
   echo "<br/><br/>";
+
   if(getenv('MY_STRING') == 'BLUE') {
     echo "<p style='color:blue;'>";
   } elseif(getenv('MY_STRING') == 'GREEN') {
     echo "<p style='color:green;'>";
   } else {
-    echo "<p>"
+    echo "<p>";
   }
   print "The value of the variable MY_STRING is " . getenv('MY_STRING') . "\n";
   echo "</p>";
